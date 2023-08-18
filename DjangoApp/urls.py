fc9785fe8d8d051aa94bc4da1238db547454afa7
Dextron <tueshin12@gmail.com>
2023-08-18 11:16:50 +0530

    more edits

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('glitter/', include('Glitter.urls')),
+    path('', include('Glitter.urls')),
 ]
