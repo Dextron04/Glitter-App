3a33df9fe14aa26680e5aabf185c77b0d3058189
Dextron <tueshin12@gmail.com>
2023-08-18 11:06:47 +0530

    corrected a few items

--- Glitter/urls.py
+++ Glitter/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from views import preTemplateView
+from Glitter.views import preTemplateView
 
 urlpatterns = [
     path('', preTemplateView, name='pretemplate'),
