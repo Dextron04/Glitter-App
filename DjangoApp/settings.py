efa0e3ba39d07fe72ed2e03081cf2e5b3daeaa14
Dextron <tueshin12@gmail.com>
2023-08-18 10:52:20 +0530

    added new files

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -1,3 +1,5 @@
+
+
 """
 Django settings for DjangoApp project.
 
@@ -9,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'DjangoApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/var/www/DjangoApp/Glitter/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from Glitter.views import display_user_options, handle_invalid_url, display_about, display_home
+from Glitter.views import preTemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', preTemplateView, name='pretemplate'),
+    path('home/', display_home, name='display_home'),
+    path('about/', display_about, name='display_about'),
+    path('filter/', display_user_options, name="display_user_options"),
+    path('<str:invalid_url>/', handle_invalid_url, name='handle_invalid_url'),
+    path('filter/<str:invalid_url>/', handle_invalid_url, name='handle_invalid_url'),
+    path('about/<str:invalid_url>/', handle_invalid_url, name='handle_invalid_url')
 ]
